88898a42b473e6b7f455f52ad36a5fb9cea4f27e
jtagrgh <jakob.koblinsky@gmail.com>
2022-12-07 17:13:29 -0500

    make stack better

--- day7/stack.cpp
+++ day7/stack.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <sstream>
 #include <stack>
-#include <utility>
 
 using namespace std;
 
@@ -16,36 +15,35 @@ int last_sum;
 
 int main() {
     input.open("in2.txt");
-    int namer {0};
-    stack<pair<int, int>> call_stack;
+    stack<int> call_stack;
 
     while (!getline(input, line).eof()) {
         line_s.str(line + " ");
         line_s.clear(); // Clear eof bit
         while (!getline(line_s, word, ' ').eof()) {
             if (isdigit(word[0])) { // File in dir, add size to sum
-                call_stack.top().second += stoi(word);
+                call_stack.top() += stoi(word);
             }
             else if (word == "cd") {
                 getline(line_s, word, ' ');
                 if (word == "..") { // Dir finished, return sum
-                    int sum = call_stack.top().second;
+                    int sum = call_stack.top();
                     if (sum < 100000) total += sum;
                     call_stack.pop();
-                    call_stack.top().second += sum;
+                    call_stack.top() += sum;
                 } else { // Dir in dir, add dir size to sum
-                    call_stack.push(pair<int,int>{namer++, 0});
+                    call_stack.push({0});
                 }
             }
         }
     }
 
     while (1) {
-        int sum = call_stack.top().second;
+        int sum = call_stack.top();
         if (sum < 100000) total += sum;
         call_stack.pop();
         if (call_stack.empty()) break;
-        call_stack.top().second += sum;
+        call_stack.top() += sum;
     }
 
     cout << total << endl;
